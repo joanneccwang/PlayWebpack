4302891e4b15616a84e08570880c6be2821a6d08
JoanneWang <joannewang@emotibot.com>
2019-10-29 18:05:23 +0800

    Webpack: Asset Management

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // Install lodash package and import it for webpack solution
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
   const element = document.createElement('div');
@@ -8,6 +11,15 @@ function component() {
   // BAD: We wouldn't know apparently it depends on external library
   
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add image to our existing div
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
 
   return element;
 }
