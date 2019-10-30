74187172d849c908a19fd3298159c91ded7b3578
JoanneWang <joannewang@emotibot.com>
2019-10-30 11:50:37 +0800

    Clear up for next tutorial

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 // Install lodash package and import it for webpack solution
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
   const element = document.createElement('div');
@@ -11,16 +8,6 @@ function component() {
   // BAD: We wouldn't know apparently it depends on external library
   
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add image to our existing div
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,41 +4,6 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,   // Test if filename regex matches 
-        use: [            // Use the following loaders to handle file
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader',
-        ],
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader',
-        ],
-      },
-    ],
+    path: path.resolve(__dirname, 'dist'),  // This should be an absolute path
   },
 };
